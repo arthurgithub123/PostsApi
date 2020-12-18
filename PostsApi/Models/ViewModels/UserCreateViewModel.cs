008704d66d212e27cf326074978058a170ed9d8a
Arthur <arthuromp000@gmail.com>
2020-12-17 23:20:33 -0300

    Add UserCreateViewModel and UserLoginViewModel

---
+++ PostsApi/Models/ViewModels/UserCreateViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace PostsApi.Models.ViewModels
+{
+    public class UserCreateViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public IFormFile Avatar { get; set; }
+    }
+}
---
+++ PostsApi/Models/ViewModels/UserLoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PostsApi.Models.ViewModels
+{
+    public class UserLoginViewModel
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
