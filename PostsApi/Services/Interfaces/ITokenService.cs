3f2b6f9976fcfc419fb60b60be4cf12656facb02
Arthur <arthuromp000@gmail.com>
2020-12-17 23:18:59 -0300

    Remove unnecessary usings from ITokenService

--- PostsApi/Services/Interfaces/ITokenService.cs
+++ PostsApi/Services/Interfaces/ITokenService.cs
@@ -1,9 +1,5 @@
 ﻿using PostsApi.Models.Entities.Identity;
 using PostsApi.Models.Token;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace PostsApi.Services.Interfaces
 {
