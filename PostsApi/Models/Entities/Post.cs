607bd617babf4d067809731e07c4a617926af7f3
Arthur <arthuromp000@gmail.com>
2021-01-02 02:49:11 -0300

    Add Post class

---
+++ PostsApi/Models/Entities/Post.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using PostsApi.Models.Entities.Identity;
+
+namespace PostsApi.Models.Entities
+{
+    public class Post : BaseModel
+    {
+        public string Description { get; set; }
+        public IFormFile Image { get; set; }
+        public virtual ApplicationUser User { get; set; }
+    }
+}
