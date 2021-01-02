c134d317341ca35ff49aeb69fb7bde5930e9f1bc
Arthur <arthuromp000@gmail.com>
2021-01-02 02:52:11 -0300

    Add BaseModel class

---
+++ PostsApi/Models/Entities/BaseModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PostsApi.Models.Entities
+{
+    public class BaseModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public Guid? CreatorId { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? EditedAt { get; set; }
+        public Guid? EditorId { get; set; }
+        public DateTime? ExcludedAt { get; set; }
+        public Guid? ExcludeId { get; set; }
+    }
+}
