a61487c3642a9043a03f192c102f75fd9abd8df7
Arthur <arthuromp000@gmail.com>
2021-01-07 13:51:54 -0300

    Add migration for post

---
+++ PostsApi/Migrations/20210107164820_Add-post.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PostsApi.Migrations
+{
+    public partial class Addpost : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatorId = table.Column<Guid>(nullable: true),
+                    CreatedAt = table.Column<DateTime>(nullable: true),
+                    EditedAt = table.Column<DateTime>(nullable: true),
+                    EditorId = table.Column<Guid>(nullable: true),
+                    ExcludedAt = table.Column<DateTime>(nullable: true),
+                    ExcludeId = table.Column<Guid>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    UserId = table.Column<Guid>(nullable: true),
+                    RecomendUserId = table.Column<Guid>(nullable: true),
+                    RecomendDate = table.Column<DateTime>(nullable: true),
+                    AcceptedAt = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Posts_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_UserId",
+                table: "Posts",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}
