533c59bdab295a23d1940bb2be8f777b58e8d214
Arthur <arthuromp000@gmail.com>
2021-01-07 13:52:55 -0300

    Add NotMapped attribute for post image property

--- PostsApi/Models/Entities/Post.cs
+++ PostsApi/Models/Entities/Post.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.AspNetCore.Http;
 using PostsApi.Models.Entities.Identity;
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PostsApi.Models.Entities
 {
     public class Post : BaseModel
     {
         public string Description { get; set; }
+        [NotMapped]
         public IFormFile Image { get; set; }
         public virtual ApplicationUser User { get; set; }
         public Guid? RecomendUserId { get; set; }
