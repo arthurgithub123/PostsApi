ccd94593a3e951a556e7f9d48dbba069b07ad059
Arthur <arthuromp000@gmail.com>
2021-01-09 19:23:13 -0300

    Add migration for post imageName

---
+++ PostsApi/Migrations/20210109221331_Add-imageName-to-post.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PostsApi.Migrations
+{
+    public partial class AddimageNametopost : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageName",
+                table: "Posts",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageName",
+                table: "Posts");
+        }
+    }
+}
