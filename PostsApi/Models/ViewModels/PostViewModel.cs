a5e33d9acf3cfb45ad049297a33008a4860d2d87
Arthur <arthuromp000@gmail.com>
2021-01-12 13:27:03 -0300

    Add PostViewModel

---
+++ PostsApi/Models/ViewModels/PostViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+
+namespace PostsApi.Models.ViewModels
+{
+    public class PostViewModel
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+        public IFormFile Image { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? RecomendDate { get; set; }
+    }
+}
