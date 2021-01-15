ddeed0b9c81871bbc9567fdd126e7a2edb347456
Arthur <arthuromp000@gmail.com>
2021-01-14 22:11:38 -0300

    Add migrations for changes to Product properties

--- PostsApi/Migrations/20210115011044_Change-some-Product-properties.Designer.cs
+++ PostsApi/Migrations/20210115011044_Change-some-Product-properties.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PostsApi.Context;
 
 namespace PostsApi.Migrations
 {
     [DbContext(typeof(PostsContext))]
-    partial class PostsContextModelSnapshot : ModelSnapshot
+    [Migration("20210115011044_Change-some-Product-properties")]
+    partial class ChangesomeProductproperties
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PostsApi/Migrations/20210115011044_Change-some-Product-properties.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PostsApi.Migrations
+{
+    public partial class ChangesomeProductproperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RecomendDate",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "RecomendUserId",
+                table: "Posts");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "AcceptedUserId",
+                table: "Posts",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCreatedByAdmin",
+                table: "Posts",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AcceptedUserId",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "IsCreatedByAdmin",
+                table: "Posts");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RecomendDate",
+                table: "Posts",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "RecomendUserId",
+                table: "Posts",
+                type: "uniqueidentifier",
+                nullable: true);
+        }
+    }
+}
