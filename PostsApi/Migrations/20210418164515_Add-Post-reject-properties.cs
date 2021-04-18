2fb30092dced6dea722029136f11db3ef34f6fc9
Arthur <arthuromp000@gmail.com>
2021-04-18 13:47:30 -0300

    Add migration files

---
+++ PostsApi/Migrations/20210418164515_Add-Post-reject-properties.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PostsApi.Migrations
+{
+    public partial class AddPostrejectproperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RejectedAt",
+                table: "Posts",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "RejectedUserId",
+                table: "Posts",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RejectedAt",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "RejectedUserId",
+                table: "Posts");
+        }
+    }
+}
