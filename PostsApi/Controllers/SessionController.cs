cbe09f8d9bc796f91977f96e20bb5c51b93aec29
Arthur <arthuromp000@gmail.com>
2021-06-17 20:02:13 -0300

    Remove unused imports

--- PostsApi/Controllers/SessionController.cs
+++ PostsApi/Controllers/SessionController.cs
@@ -1,13 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using PostsApi.Models.Entities.Identity;
 using PostsApi.Models.Token;
 using PostsApi.Models.ViewModels.User;
 using PostsApi.Services.Interfaces;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace PostsApi.Controllers
