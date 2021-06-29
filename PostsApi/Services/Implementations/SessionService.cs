ade5a6843453fa2ea077bc836901bca7b253e6aa
Arthur <arthuromp000@gmail.com>
2021-06-29 17:37:04 -0300

    Change CreateAdministrator logic for sending email for password creation

--- PostsApi/Services/Implementations/SessionService.cs
+++ PostsApi/Services/Implementations/SessionService.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using PostsApi.ErrorHandling;
+using PostsApi.Models.Email;
 using PostsApi.Models.Entities.Identity;
 using PostsApi.Models.Token;
 using PostsApi.Models.ViewModels.User;
 using PostsApi.Services.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -33,14 +35,14 @@ namespace PostsApi.Services.Implementations
         private readonly IConfiguration _configuration;
         private readonly IUserService _userService;
 
-        public async Task<UserToken> CreateAdministrator(UserCreateViewModel userCreateViewModel, bool isModelStateValid)
+        public async Task CreateAdministrator(AdminCreateViewModel adminCreateViewModel, bool isModelStateValid)
         {
             if (!isModelStateValid)
             {
                 throw new HttpResponseException(400, "E-mail e senha devem ser informados");
             }
 
-            ApplicationUser emailExists = await _userManager.FindByEmailAsync(userCreateViewModel.Email);
+            ApplicationUser emailExists = await _userManager.FindByEmailAsync(adminCreateViewModel.Email);
 
             if (emailExists != null)
             {
@@ -49,19 +51,19 @@ namespace PostsApi.Services.Implementations
 
             ApplicationUser applicationUser = new ApplicationUser
             {
-                Name = userCreateViewModel.Name,
-                Email = userCreateViewModel.Email,
-                UserName = userCreateViewModel.Email
+                Name = adminCreateViewModel.Name,
+                Email = adminCreateViewModel.Email,
+                UserName = adminCreateViewModel.Email
             };
 
-            if (userCreateViewModel.Avatar != null && userCreateViewModel.Avatar.Length > 0)
+            if (adminCreateViewModel.Avatar != null && adminCreateViewModel.Avatar.Length > 0)
             {
-                string imageFileName = _userService.SaveProfileImage(userCreateViewModel.Avatar);
+                string imageFileName = _userService.SaveProfileImage(adminCreateViewModel.Avatar);
 
                 applicationUser.Avatar = imageFileName;
             }
 
-            var createResult = await _userManager.CreateAsync(applicationUser, userCreateViewModel.Password);
+            var createResult = await _userManager.CreateAsync(applicationUser, Guid.NewGuid().ToString() + "A-Z");
 
             if (!createResult.Succeeded)
             {
@@ -70,26 +72,36 @@ namespace PostsApi.Services.Implementations
             else
             {
                 await _userManager.AddToRoleAsync(applicationUser, "Administrator");
-
-                var signInResult = await _signInManager.PasswordSignInAsync(
-                    userCreateViewModel.Email,
-                    userCreateViewModel.Password,
-                    isPersistent: false,
-                    lockoutOnFailure: false
-                );
-
-                if (!signInResult.Succeeded)
-                {
-                    throw new HttpResponseException(400, "Login inválido");
-                }
-                else
-                {
-                    IList<string> userRoles = await _userManager.GetRolesAsync(applicationUser);
-                    string jwtSecretKey = _configuration["JWT:Key"];
-
-                    return _tokenService.BuildToken(applicationUser, userRoles[0], jwtSecretKey);
-                }
             }
+
+            string resetToken = await _userManager.GeneratePasswordResetTokenAsync(applicationUser);
+
+            if (string.IsNullOrEmpty(resetToken))
+            {
+                throw new HttpResponseException(500, "Erro ao gerar token para criar a senha");
+            }
+
+            string resetPageWithTokenAndEmail = string.Concat("http://localhost:3000/users/create_password", "?token=", resetToken, "&email=", adminCreateViewModel.Email);
+
+            string email = _configuration["Email:Email"];
+            string password = _configuration["Email:Password"];
+            string name = _configuration["Email:Name"];
+
+            string subject = "Posts API - Criação de senha";
+            
+            string shortLinkDescription = resetPageWithTokenAndEmail.Substring(0, 80);
+
+            string htmlBody = string.Concat(
+                "<div style='text-align: center;'>",
+                    "<p>Olá, ", adminCreateViewModel.Name, "</p>",
+                    "<p>Clique no link abaixo para criar sua senha: <p>",
+                    "<p><a href='", resetPageWithTokenAndEmail, "'>", shortLinkDescription, "</a></p>",
+                    "<p>Obrigado, </p>",
+                    "<p>Equipe Posts API</p>",
+                "</div>"
+            );
+            
+            EmailHelper.SendEmail(name, email, adminCreateViewModel.Name, adminCreateViewModel.Email, subject, htmlBody, email, password);            
         }
 
         public async Task<UserToken> CreateCommom(UserCreateViewModel userCreateViewModel, bool isModelStateValid)
