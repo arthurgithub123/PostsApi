df15c060f477fb53f36748a0a89d46bd2c882ca8
Arthur <arthuromp000@gmail.com>
2021-06-29 17:30:51 -0300

    Add view model classes for creating administrator password using email

---
+++ PostsApi/Models/ViewModels/User/AdminCreateViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace PostsApi.Models.ViewModels.User
+{
+    public class AdminCreateViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public IFormFile Avatar { get; set; }
+    }
+}
---
+++ PostsApi/Models/ViewModels/User/PasswordCreateViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PostsApi.Models.ViewModels.User
+{
+    public class PasswordCreateViewModel
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Token { get; set; }
+    }
+}
