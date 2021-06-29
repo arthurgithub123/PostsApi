9af18a4e2b5d0767c6e700492e159593215359e3
Arthur <arthuromp000@gmail.com>
2021-06-29 16:38:07 -0300

    Add EmailHelper for sending emails

---
+++ PostsApi/Models/Email/EmailHelper.cs
@@ -0,0 +1,33 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using MimeKit.Text;
+
+namespace PostsApi.Models.Email
+{
+    public class EmailHelper
+    {
+        public static void SendEmail(string fromName, string fromAddress, string toName, string toAddress, string subject, string htmlBody, string senderEmail, string senderPassword)
+        {
+			var message = new MimeMessage();
+		
+			message.From.Add(new MailboxAddress(fromName, fromAddress));
+			message.To.Add(new MailboxAddress(toName, toAddress));
+			message.Subject = subject;
+
+			message.Body = new TextPart(TextFormat.Html) { Text = htmlBody };
+
+			using (var smtpClient = new SmtpClient())
+			{
+				smtpClient.CheckCertificateRevocation = false;
+
+				smtpClient.Connect("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
+
+				smtpClient.Authenticate(senderEmail, senderPassword);
+
+				smtpClient.Send(message);
+				smtpClient.Disconnect(true);
+			}
+		}
+    }
+}
